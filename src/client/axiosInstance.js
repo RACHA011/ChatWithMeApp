ed7afc15b4f4ce35eb2e9d21c4998b6ab6fec368
Racha011 <ratshalingwaadivhaho106@gmail.com>
2025-01-28 10:17:58 +0200

    deployment updates

--- src/client/axiosInstance.js
+++ src/client/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "http://192.168.100.59:8080", 
+  // baseURL: "http://192.168.100.59:8080", 
+  baseURL: "https://chatwithmeapp.onrender.com", 
   timeout: 50000, 
   headers: {
     "Content-Type": "application/json",
